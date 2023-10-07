13d94668fb4a39c3bd8385881ddb8dcc034e087f
Juan Carrillo <ing.carrillo12@gmail.com>
2023-10-07 12:07:29 -0500

    Js terminado

--- js/app.js
+++ js/app.js
@@ -1,13 +1,135 @@
-var pagination = {prev: "", next: ""}
+var page = 1
+const baseUrl = "https://rickandmortyapi.com/api/character"
 const cardContainer = document.querySelector(".card-list-character")
-const fetchApi= async()=>{
+const formSearch = document.querySelector(".form-search")
+const iconFilter= document.querySelector(".icon-filter")
+const filter= document.querySelector(".filter")
+const select = document.querySelector(".contenido-types")
+const pagination = document.querySelector(".container-pagination")
+const fetchApi= async(page)=>{
+    cardContainer.innerHTML = "";
+    pagination.style.display = "flex"
     try {
-        const response = await fetch("https://rickandmortyapi.com/api/character")
+        const response = await fetch(`${baseUrl}?page=${page}`)
         if(!response.ok)throw new Error("Error en la red")
-        const {results, info} = await response.json()
-        const {prev,next} = await info
-        pagination = {prev, next}
-        results.forEach(({id, image, name, species, gender }) => {
+        const {results} = await response.json()
+        results.forEach(({id, image, name, species, gender, location, origin }) => {
+        cardContainer.innerHTML += `
+        <a href="" class="card-character">
+            <div class="character-img">
+                <img src="${image}" alt="bulbasaur">
+            </div>
+            <div class="character-info">
+                <span class="character-id">N°${id}</span>
+                <h3 class="character-name text-capitalize">${name}</h3>
+                <div class="character-types">
+                    <div class="group-type">
+                        <span class="type "><b>Specie:</b>${species}</span>
+                    </div>
+                    <div class="group-type">
+                        <span class="type"><b>Gender:</b>${gender}</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+        `
+        });
+    } catch (error) {
+        console.log("Error: ", error)
+    }
+}
+const searchInApi = async ()=>{
+    try {
+        const characterSearch = await document.querySelector(".input-search").value;
+        let allCharacters= [];
+        for (let page = 1; page < 42; page++) {
+            const response = await fetch(`${baseUrl}?page=${page}`)
+            if(!response.ok)throw new Error("Error en la red");
+            const {results} = await response.json();
+            allCharacters = await results.filter(character=>character.name.includes(characterSearch));
+            return allCharacters;
+        }
+    } catch (error) {
+        console.log("ERROR: ", error)
+    }
+}
+const getNamesApi = async()=>{
+    try {    
+        let allNames= [];
+        for (let page = 1; page < 42; page++) {
+            const response = await fetch(`${baseUrl}?page=${page}`)
+            if(!response.ok)throw new Error("Error en la red");
+            const {results} = await response.json();
+           
+            const names = results.map((character)=>{
+                    const {name, id}= character
+                    return {name, id}
+                })
+            allNames = allNames.concat(names)
+           
+        }
+        allNames.forEach(({name, id})=>{
+            select.innerHTML += `<option value="${id}" id="character">${name}</option>` 
+        })
+    } catch (error) {
+        console.log("ERROR: ", error)
+    }
+}
+const findCharacterApi = async(id)=>{
+    try {
+        cardContainer.innerHTML = "";
+        const response = await fetch(`${baseUrl}/${id}`)
+        if(!response.ok)throw new Error("Error en la red")
+        const data = await response.json()
+        cardContainer.innerHTML += `
+        <a href="" class="card-character">
+            <div class="character-img">
+                <img src="${data.image}" alt="bulbasaur">
+            </div>
+            <div class="character-info">
+                <span class="character-id">N°${data.id}</span>
+                <h3 class="character-name text-capitalize">${data.name}</h3>
+                <div class="character-types">
+                    <div class="group-type">
+                        <span class="type "><b>Specie:</b>${data.species}</span>
+                    </div>
+                    <div class="group-type">
+                        <span class="type"><b>Gender:</b>${data.gender}</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+        `
+    } catch (error) {
+        console.log("ERROR ",error)
+    }
+}
+const nextPage =()=>{
+    page= page + 1
+    fetchApi(page)
+    
+}
+const prevPage = ()=>{
+    page = page - 1 ;
+    fetchApi(page)
+}
+const changeFilter =()=>{
+    filter.classList.toggle("active")
+}
+select.addEventListener("change", ()=>{
+    if(select.value !== "all"){
+        findCharacterApi(select.value)
+    }else{
+        fetchApi()
+    }
+    
+})
+
+formSearch.addEventListener("submit", async(e)=>{
+    cardContainer.innerHTML = "";
+    e.preventDefault()
+    const foundCharacters = await searchInApi()
+    foundCharacters.forEach(({id, image, name, species, gender }) => {
         cardContainer.innerHTML += `
         <a href="" class="card-character">
             <div class="character-img">
@@ -23,70 +145,8 @@ const fetchApi= async()=>{
             </div>
         </a>
         `
+        pagination.style.display = "none"
         });
-    } catch (error) {
-        console.log("Error: ", error)
-    }
-}
-const nextPage = async ()=>{
-  cardContainer.innerHTML = "";
-  try {
-    const page= pagination;
-    const response = await fetch(page.next);
-    if(!response.ok)throw new Error("Error en la red")
-    const {results, info} = await response.json()
-    const {prev, next} = await info
-    pagination = {prev, next}
-    results.forEach(({id, image, name, species, gender }) => {
-    cardContainer.innerHTML += `
-    <a href="" class="card-character">
-        <div class="character-img">
-            <img src="${image}" alt="bulbasaur">
-        </div>
-        <div class="character-info">
-            <span class="character-id">${id}</span>
-            <h3 class="character-name text-capitalize">${name}</h3>
-            <div class="character-types">
-                <span class="type grass">${species}</span>
-                <span class="type poison">${gender}</span>
-            </div>
-        </div>
-    </a>
-    `
-    });
-  } catch (error) {
-    console.log("Error: ", error)
-  }
-}
-const prevPage = async ()=>{
-  cardContainer.innerHTML = "";
-  try {
-    const page= pagination
-    const response = await fetch(page.prev);
-    if(!response.ok)throw new Error("Error en la red")
-    const {results, info} = await response.json()
-    const {prev, next} = await info
-    pagination = {prev, next}
-    results.forEach(({id, image, name, species, gender }) => {
-    cardContainer.innerHTML += `
-    <a href="" class="card-character">
-        <div class="character-img">
-            <img src="${image}" alt="bulbasaur">
-        </div>
-        <div class="character-info">
-            <span class="character-id">${id}</span>
-            <h3 class="character-name text-capitalize">${name}</h3>
-            <div class="character-types">
-                <span class="type grass">${species}</span>
-                <span class="type poison">${gender}</span>
-            </div>
-        </div>
-    </a>
-    `
-    });
-  } catch (error) {
-    console.log("Error: ", error)
-  }
-}
-
+})
 fetchApi()
+getNamesApi()
